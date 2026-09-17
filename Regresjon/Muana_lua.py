bb2d61aaf352a864cc697a65ea8e7d900170bb8f
Leonid-portal <Leonsorvanov@outlook.com>
2026-09-18 01:11:26 +0200

    Update Muana_lua.py

--- Regresjon/Muana_lua.py
+++ Regresjon/Muana_lua.py
@@ -7,18 +7,18 @@ plt.title("CO2 levels in Muana lua")
 plt.xlabel("Years")
 plt.ylabel("CO2, ppm")
 plt.grid()
-plt.scatter(X,Y)
+plt.scatter(X,Y, color="#014D4E")
 
 N=len(X)
 
-#formula for calculating linear regresion
+
 X_bar = sum(X)/N
 Y_bar = sum(Y)/N
-
+#formula for calculating linear regresion (taken from lecture notes)
 b = sum((X-X_bar)*(Y-Y_bar)) / sum((X-X_bar)**2)
 a = Y_bar - b * X_bar
 X_Y_reg = a + b*X
 print(f"a = {a:.2f}, b = {b:.2f}")
 
-plt.plot(X, X_Y_reg, "r-")
+plt.plot(X, X_Y_reg, color="#8B0000")
 plt.show()
