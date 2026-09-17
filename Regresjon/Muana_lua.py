db3cd18f007df2d2e94ed7f6b4ff5e4e9644ef84
Leonid-portal <Leonsorvanov@outlook.com>
2026-09-18 01:01:02 +0200

    made the code for plotting muana lua co2 ppm

---
+++ Regresjon/Muana_lua.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.close('all')
+#X is years, Y is CO2-levels in ppm
+X, Y = np.loadtxt("Regresjon/Datasett2.dat", unpack=True)
+plt.title("CO2 levels in Muana lua")
+plt.xlabel("Years")
+plt.ylabel("CO2, ppm")
+plt.grid()
+plt.scatter(X,Y)
+
+N=len(X)
+
+#formula for calculating linear regresion
+X_bar = sum(X)/N
+Y_bar = sum(Y)/N
+
+b = sum((X-X_bar)*(Y-Y_bar)) / sum((X-X_bar)**2)
+a = Y_bar - b * X_bar
+X_Y_reg = a + b*X
+print(f"a = {a:.2f}, b = {b:.2f}")
+
+plt.plot(X, X_Y_reg, "r-")
+plt.show()
--- Regresjon/plotting_test.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-X, Y = np.loadtxt("Datasett.dat", unpack=True)
-plt.scatter(X,Y)
-plt.show()
